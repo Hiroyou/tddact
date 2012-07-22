07501409672099d74d958b324770c5a512edb1f3
sue445 <sue445@sue445.net>
2012-07-22 13:10:17 +0900

    refactor test

--- vending_machine_spec.rb
+++ vending_machine_spec.rb
@@ -6,27 +6,27 @@ describe VendingMachine do
   describe '#add' do
     it 'add yen10' do
       subject.add(:yen10)
-      subject.moneys.should include(:yen10)
+      subject.moneys[:yen10].should == 1
     end
 
     it 'add yen50' do
       subject.add(:yen50)
-      subject.moneys.should include(:yen50)
+      subject.moneys[:yen50].should == 1
     end
 
     it 'add yen100' do
       subject.add(:yen100)
-      subject.moneys.should include(:yen100)
+      subject.moneys[:yen100].should == 1
     end
 
     it 'add yen500' do
       subject.add(:yen500)
-      subject.moneys.should include(:yen500)
+      subject.moneys[:yen500].should == 1
     end
 
     it 'add yen1000' do
       subject.add(:yen1000)
-      subject.moneys.should include(:yen1000)
+      subject.moneys[:yen1000].should == 1
     end
 
     it 'add yen10 and yen100' do
