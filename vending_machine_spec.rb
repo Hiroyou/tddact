c6d6ea4f902ef1632642eff81bc24308aafd783a
Hiroyou <hyshhryk@gmail.com>
2012-07-22 12:03:01 +0900

    replace tab with space

--- vending_machine_spec.rb
+++ vending_machine_spec.rb
@@ -1,39 +1,39 @@
 require './vending_machine'
 
 describe VendingMachine do
-	subject { VendingMachine.new }
+  subject { VendingMachine.new }
 
   describe '#add' do
     it 'add yen10' do
       subject.add(:yen10)
       subject.moneys.should include(:yen10)
     end
-    
-		it 'add yen50' do
+
+    it 'add yen50' do
       subject.add(:yen50)
       subject.moneys.should include(:yen50)
     end
-    
-		it 'add yen100' do
+
+    it 'add yen100' do
       subject.add(:yen100)
       subject.moneys.should include(:yen100)
     end
-    
-		it 'add yen500' do
+
+    it 'add yen500' do
       subject.add(:yen500)
       subject.moneys.should include(:yen500)
     end
-		
-		it 'add yen1000' do
+
+    it 'add yen1000' do
       subject.add(:yen1000)
       subject.moneys.should include(:yen1000)
     end
 
-		it 'add yen10 and yen100' do
+    it 'add yen10 and yen100' do
       subject.add(:yen10)
       subject.add(:yen100)
       subject.moneys.should include(:yen10)
       subject.moneys.should include(:yen100)
-		end
+    end
   end
 end
