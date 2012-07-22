879744b7cbd62e78197d2822a1daa64d4c9b612d
sue445 <sue445@sue445.net>
2012-07-22 11:56:34 +0900

    refactor: use subject

--- vending_machine_spec.rb
+++ vending_machine_spec.rb
@@ -1,35 +1,32 @@
 require './vending_machine'
 
 describe VendingMachine do
+	subject { VendingMachine.new }
+
   describe '#add' do
     it 'add yen10' do
-      vm = VendingMachine.new
-      vm.add(:yen10)
-      vm.moneys.should include(:yen10)
+      subject.add(:yen10)
+      subject.moneys.should include(:yen10)
     end
     
 		it 'add yen50' do
-      vm = VendingMachine.new
-      vm.add(:yen50)
-      vm.moneys.should include(:yen50)
+      subject.add(:yen50)
+      subject.moneys.should include(:yen50)
     end
     
 		it 'add yen100' do
-      vm = VendingMachine.new
-      vm.add(:yen100)
-      vm.moneys.should include(:yen100)
+      subject.add(:yen100)
+      subject.moneys.should include(:yen100)
     end
     
 		it 'add yen500' do
-      vm = VendingMachine.new
-      vm.add(:yen500)
-      vm.moneys.should include(:yen500)
+      subject.add(:yen500)
+      subject.moneys.should include(:yen500)
     end
 		
 		it 'add yen1000' do
-      vm = VendingMachine.new
-      vm.add(:yen1000)
-      vm.moneys.should include(:yen1000)
+      subject.add(:yen1000)
+      subject.moneys.should include(:yen1000)
     end
   end
 end
