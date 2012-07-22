5544b9e47327563d7c7e1ac4edfe684317d65bfc
sue445 <sue445@sue445.net>
2012-07-22 11:51:24 +0900

    add tests

--- vending_machine_spec.rb
+++ vending_machine_spec.rb
@@ -2,10 +2,34 @@ require './vending_machine'
 
 describe VendingMachine do
   describe '#add' do
-    it 'add yen100' do
+    it 'add yen10' do
+      vm = VendingMachine.new
+      vm.add(:yen10)
+      vm.moneys.should include(:yen10)
+    end
+    
+		it 'add yen50' do
+      vm = VendingMachine.new
+      vm.add(:yen50)
+      vm.moneys.should include(:yen50)
+    end
+    
+		it 'add yen100' do
       vm = VendingMachine.new
       vm.add(:yen100)
       vm.moneys.should include(:yen100)
+    end
+    
+		it 'add yen500' do
+      vm = VendingMachine.new
+      vm.add(:yen500)
+      vm.moneys.should include(:yen500)
+    end
+		
+		it 'add yen1000' do
+      vm = VendingMachine.new
+      vm.add(:yen1000)
+      vm.moneys.should include(:yen1000)
     end
   end
 end
